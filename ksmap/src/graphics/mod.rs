865d0a604000068ebd6214162ce4fe3106e9815a
spindlymist <ocrobin@gmail.com>
2026-01-19 20:56:14 -0800

    Cache images by path

--- ksmap/src/graphics/mod.rs
+++ ksmap/src/graphics/mod.rs
@@ -1,5 +1,5 @@
 use std::{
-    collections::HashMap,
+    collections::{HashMap, hash_map::Entry},
     fs::OpenOptions,
     io::{self, BufReader},
     path::{Path, PathBuf},
@@ -14,10 +14,12 @@ use crate::definitions::{ObjectDef, ObjectDefs, ObjectId, ObjectKind};
 
 mod png_decoder;
 
+type MaybeImage = Option<Rc<RgbaImage>>;
+
 pub struct Graphics<'a> {
     paths: Paths,
     object_defs: &'a ObjectDefs,
-    paths_loaded: HashMap<PathBuf, Rc<RgbaImage>>,
+    cache: HashMap<(PathBuf, MagicColor), MaybeImage>,
     tilesets: HashMap<AssetId, Rc<RgbaImage>>,
     gradients: HashMap<AssetId, Rc<RgbaImage>>,
     objects: HashMap<ObjectId, Rc<RgbaImage>>,
@@ -63,47 +65,13 @@ impl<'a> Graphics<'a> {
         Self {
             paths,
             object_defs,
-            paths_loaded: HashMap::new(),
+            cache: HashMap::new(),
             tilesets: HashMap::new(),
             gradients: HashMap::new(),
             objects: HashMap::new(),
         }
     }
     
-    pub fn load_tilesets(&mut self, ids: &[AssetId]) -> Result<()> {
-        for id in ids {
-            if let Some(image) = load_tileset(&self.paths, *id)? {
-                self.tilesets.insert(*id, Rc::new(image));
-            }
-        }
-        Ok(())
-    }
-    
-    pub fn load_gradients(&mut self, ids: &[AssetId]) -> Result<()> {
-        for id in ids {
-            if let Some(image) = load_gradient(&self.paths, *id)? {
-                self.gradients.insert(*id, Rc::new(image));
-            }
-        }
-        Ok(())
-    }
-    
-    pub fn load_objects(&mut self, ids: &[ObjectId]) -> Result<()> {
-        for id in ids {
-            let def = self.object_defs.get(id);
-            let image = match def.map(|def| &def.kind) {
-                Some(ObjectKind::Object) | None => load_stock_object(&self.paths, id, def)?,
-                Some(ObjectKind::CustomObject) => load_custom_object(&self.paths, def.unwrap())?,
-                Some(ObjectKind::OverrideObject(_)) =>
-                    load_override_object(&self.paths, def.unwrap(), &self.object_defs)?
-            };
-            if let Some(image) = image {
-                self.objects.insert(id.clone(), Rc::new(image));
-            }
-        }
-        Ok(())
-    }
-
     pub fn tileset(&self, id: AssetId) -> Option<&RgbaImage> {
         self.tilesets.get(&id)
             .map(Rc::as_ref)
@@ -118,131 +86,174 @@ impl<'a> Graphics<'a> {
         self.objects.get(&id)
             .map(Rc::as_ref)
     }
-}
-
-const BLACK: Rgba<u8> = Rgba([0, 0, 0, 255]);
-const MAGENTA: Rgba<u8> = Rgba([255, 0, 255, 255]);
-
-fn try_load_image(path: &Path, magic_color: Rgba<u8>, force_magic_color: bool) -> Result<Option<RgbaImage>> {
-    let decoder = {
+    
+    pub fn load_tilesets(&mut self, ids: &[AssetId]) -> Result<()> {
+        for id in ids {
+            if let Some(image) = self.load_tileset(*id)? {
+                self.tilesets.insert(*id, image);
+            }
+        }
+        Ok(())
+    }
+    
+    pub fn load_gradients(&mut self, ids: &[AssetId]) -> Result<()> {
+        for id in ids {
+            if let Some(image) = self.load_gradient(*id)? {
+                self.gradients.insert(*id, image);
+            }
+        }
+        Ok(())
+    }
+    
+    pub fn load_objects(&mut self, ids: &[ObjectId]) -> Result<()> {
+        for id in ids {
+            let def = self.object_defs.get(id);
+            let image = match def.map(|def| &def.kind) {
+                Some(ObjectKind::Object) | None => self.load_stock_object(id, def)?,
+                Some(ObjectKind::CustomObject) => self.load_custom_object(def.unwrap())?,
+                Some(ObjectKind::OverrideObject(_)) => self.load_override_object(def.unwrap())?,
+            };
+            if let Some(image) = image {
+                self.objects.insert(id.clone(), image);
+            }
+        }
+        Ok(())
+    }
+    
+    fn load_image(&mut self, path: PathBuf, magic_color: MagicColor) -> Result<MaybeImage> {
+        let cached_image = match self.cache.entry((path, magic_color)) {
+            Entry::Occupied(entry) => {
+                return Ok(entry.get()
+                    .as_ref()
+                    .map(Rc::clone))
+            },
+            Entry::Vacant(entry) => {
+                entry
+            },
+        };
+        let (path, magic_color) = cached_image.key();
+        
         let file = match OpenOptions::new().read(true).open(path) {
             Ok(file) => file,
             Err(err) if err.kind() == io::ErrorKind::NotFound => {
+                cached_image.insert(None);
                 return Ok(None);
             },
             Err(err) => Err(err)?,
         };
-        png_decoder::PngDecoder::new(BufReader::new(file))
-            .with_context(|| format!("Error while decoding {path:?}"))?
-    };
+        let decoder = png_decoder::PngDecoder::new(BufReader::new(file))
+            .with_context(|| format!("Error while decoding {path:?}"))?;
+        let image = DynamicImage::from_decoder(decoder)
+            .with_context(|| format!("Error while decoding {path:?}"))?;
 
-    let image = DynamicImage::from_decoder(decoder)
-        .with_context(|| format!("Error while decoding {path:?}"))?;
+        let is_24_bpp = matches!(image, DynamicImage::ImageRgb8(_));
+        let mut image = image.into_rgba8();
 
-    let is_24_bpp = matches!(image, DynamicImage::ImageRgb8(_));
-    let mut image = image.to_rgba8();
-
-    if is_24_bpp || force_magic_color {
-        for pixel in image.pixels_mut() {
-            if *pixel == magic_color {
-                pixel.0[0] = 0;
-                pixel.0[1] = 0;
-                pixel.0[2] = 0;
-                pixel.0[3] = 0;
+        if is_24_bpp || magic_color.force {
+            for pixel in image.pixels_mut() {
+                if *pixel == magic_color.rgba {
+                    pixel.0 = [0, 0, 0, 0];
+                }
             }
         }
+        
+        let image_rc = Rc::new(image);
+        cached_image.insert(Some(Rc::clone(&image_rc)));
+
+        Ok(Some(image_rc))
     }
 
-    Ok(Some(image))
-}
-
-fn try_load_image_from_paths(paths: &[&Path], magic_color: Rgba<u8>, force_magic_color: bool) -> Result<Option<RgbaImage>> {
-    for path in paths {
-        if let Some(image) = try_load_image(path, magic_color, force_magic_color)? {
-            return Ok(Some(image))
+    fn load_tileset(&mut self, id: AssetId) -> Result<MaybeImage> {
+        let suffix = format!("Tileset{id}.png");
+        
+        let image = self.load_image(self.paths.level_tilesets.join(&suffix), MagicColor::MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
         }
+        
+        let image = self.load_image(self.paths.data_tilesets.join(&suffix), MagicColor::MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
+        }
+        
+        Ok(None)
     }
 
-    Ok(None)
-}
-
-fn load_tileset(paths: &Paths, id: AssetId) -> Result<Option<RgbaImage>> {
-    let suffix = format!("Tileset{id}.png");
-
-    try_load_image_from_paths(&[
-        &paths.level_tilesets.join(&suffix),
-        &paths.data_tilesets.join(&suffix),
-    ], MAGENTA, false)
-}
-
-fn load_gradient(paths: &Paths, id: AssetId) -> Result<Option<RgbaImage>> {
-    let suffix = format!("Gradient{id}.png");
-
-    try_load_image_from_paths(&[
-        &paths.level_gradients.join(&suffix),
-        &paths.data_gradients.join(&suffix),
-    ], MAGENTA, false)
-}
-
-fn load_stock_object(
-    paths: &Paths,
-    ObjectId(tile, variant): &ObjectId,
-    def: Option<&ObjectDef>,
-) -> Result<Option<RgbaImage>> {
-    let suffix = match def.and_then(|def| def.path.as_ref()) {
-        Some(path) => path,
-        None => match variant {
-            Some(variant) => &format!("Bank{}/Object{}_{}.png", tile.0, tile.1, variant),
-            None => &format!("Bank{}/Object{}.png", tile.0, tile.1),
-        },
-    };
-
-    try_load_image_from_paths(&[
-        &paths.templates.join(suffix),
-        &paths.editor_objects.join(suffix),
-    ], MAGENTA, true)
-}
-
-fn load_custom_object(paths: &Paths, def: &ObjectDef) -> Result<Option<RgbaImage>> {
-    let Some(object_path) = def.path.as_ref() else {
-        return Ok(None);
-    };
-
-    let image_path = paths.custom_objects.join(object_path);
-    try_load_image(&image_path, BLACK, false)
-}
-
-fn load_override_object(
-    paths: &Paths,
-    def: &ObjectDef,
-    object_defs: &ObjectDefs,
-) -> Result<Option<RgbaImage>> {
-    let mut image = 
-        if def.ignore_oco_path {
-            let ObjectKind::OverrideObject(original_tile) = def.kind else {
-                return Ok(None);
-            };
-            let original_id = ObjectId(original_tile, None);
-            let Some(original_def) = object_defs.get(&original_id) else {
-                return Ok(None);
-            };
-            load_stock_object(paths, &original_id, Some(original_def))?
+    fn load_gradient(&mut self, id: AssetId) -> Result<MaybeImage> {
+        let suffix = format!("Gradient{id}.png");
+        
+        let image = self.load_image(self.paths.level_gradients.join(&suffix), MagicColor::MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
         }
-        else {
-            load_custom_object(paths, def)?
+        
+        let image = self.load_image(self.paths.data_gradients.join(&suffix), MagicColor::MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
+        }
+        
+        Ok(None)
+    }
+
+    fn load_stock_object(
+        &mut self,
+        id: &ObjectId,
+        def: Option<&ObjectDef>,
+    ) -> Result<MaybeImage> {
+        let ObjectId(tile, variant) = id;
+        let suffix = match def.and_then(|def| def.path.as_ref()) {
+            Some(path) => path,
+            None => match variant {
+                Some(variant) => &format!("Bank{}/Object{}_{}.png", tile.0, tile.1, variant),
+                None => &format!("Bank{}/Object{}.png", tile.0, tile.1),
+            },
         };
-
-    if def.replace_colors.is_empty() {
-        return Ok(image);
+        
+        let image = self.load_image(self.paths.templates.join(suffix), MagicColor::FORCE_MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
+        }
+        
+        let image = self.load_image(self.paths.editor_objects.join(suffix), MagicColor::FORCE_MAGENTA)?;
+        if image.is_some() {
+            return Ok(image);
+        }
+        
+        Ok(None)
     }
 
-    if let Some(image) = image.as_mut() {
-        for pixel in image.pixels_mut() {
+    fn load_custom_object(&mut self, def: &ObjectDef) -> Result<MaybeImage> {
+        let Some(path) = def.path.as_ref() else { return Ok(None) };
+        self.load_image(self.paths.custom_objects.join(path), MagicColor::BLACK)
+    }
+
+    fn load_override_object(&mut self, def: &ObjectDef) -> Result<MaybeImage> {
+        let image = 
+            if def.ignore_oco_path {
+                let ObjectKind::OverrideObject(original_tile) = def.kind else {
+                    return Ok(None);
+                };
+                let original_id = ObjectId(original_tile, None);
+                let Some(original_def) = self.object_defs.get(&original_id) else {
+                    return Ok(None);
+                };
+                self.load_stock_object(&original_id, Some(original_def))?
+            }
+            else {
+                self.load_custom_object(def)?
+            };
+            
+        if def.replace_colors.is_empty() {
+            return Ok(image);
+        }
+        let Some(image) = image else {
+            return Ok(image);
+        };
+        
+        let mut transformed_image = (*image).clone();
+        for pixel in transformed_image.pixels_mut() {
             for (old, new) in &def.replace_colors {
-                if pixel.0[0] == old[0]
-                    && pixel.0[1] == old[1]
-                    && pixel.0[2] == old[2]
-                {
+                if &pixel.0[0..3] == &old[0..3] {
                     pixel.0[0] = new[0];
                     pixel.0[1] = new[1];
                     pixel.0[2] = new[2];
@@ -250,7 +261,29 @@ fn load_override_object(
                 }
             }
         }
+        
+        Ok(Some(Rc::new(transformed_image)))
     }
 
-    Ok(image)
+}
+
+#[derive(Debug, PartialEq, Eq, Hash)]
+struct MagicColor {
+    rgba: Rgba<u8>,
+    force: bool,
+}
+
+impl MagicColor {
+    const BLACK: MagicColor = MagicColor {
+        rgba: Rgba([0, 0, 0, 255]),
+        force: false,
+    };
+    const MAGENTA: MagicColor = MagicColor {
+        rgba: Rgba([255, 0, 255, 255]),
+        force: false,
+    };
+    const FORCE_MAGENTA: MagicColor = MagicColor {
+        rgba: Rgba([255, 0, 255, 255]),
+        force: true,
+    };
 }
