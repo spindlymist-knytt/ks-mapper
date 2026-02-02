d9c62c196d7064587a611e4238bce7efb95c5ee6
spindlymist <ocrobin@gmail.com>
2026-02-01 17:14:40 -0800

    More CLI tweaks

--- cli/src/cli.rs
+++ cli/src/cli.rs
@@ -9,12 +9,12 @@ pub struct Cli {
     #[arg(short = 's', long)]
     pub seed: Option<String>,
     /// The maximum width of a single output image in pixels
-    #[arg(short = 'x', long, default_value = "48000")]
+    #[arg(short = 'x', long, default_value = "60000")]
     pub max_width: u64,
     /// The maximum height of a single output image in pixels
     #[arg(short = 'y', long, default_value = "48000")]
     pub max_height: u64,
-    /// How to break up large maps that won't fit in one image
+    /// How to divide large maps that don't fit into one image
     #[arg(value_enum, short = 'p', long, default_value = "islands")]
     pub partitioner: PartitionStrategy,
     #[command(flatten)]
@@ -27,7 +27,8 @@ pub struct Cli {
     /// Always pick a random laser phase (red/green) rather than the one with the most lasers
     #[arg(long)]
     pub randomize_lasers: bool,
-    /// Path to the KS data directory. If unspecified, it will be located relative to the level directory
+    /// Path to the KS data directory.
+    /// If unspecified, it will be located relative to the level directory
     #[arg(long = "data", help_heading = "Paths")]
     pub data_dir: Option<PathBuf>,
     /// Path to the directory containing object templates
@@ -36,7 +37,8 @@ pub struct Cli {
     /// Path to the file containing object definitions
     #[arg(long = "definitions", default_value = "mapper_objects.toml", help_heading = "Paths")]
     pub object_definitions: PathBuf,
-    /// The file or directory (if there are multiple partitions) to output to. If unspecified, it will be `Level Author - Level Name`
+    /// The file or directory (if there are multiple partitions) to output to.
+    /// If unspecified, it will be `Level Author - Level Name`
     #[arg(short, long = "output")]
     pub output_dir: Option<PathBuf>,
     /// Path to the level's directory or Map.bin
@@ -45,20 +47,21 @@ pub struct Cli {
 
 #[derive(Clone, Copy, Default, clap::ValueEnum)]
 pub enum PartitionStrategy {
-    /// Break the map up into clusters of screens that are near one another. Islands that are still too large will be
-    /// broken up using a fixed grid
+    /// Divide the map into clusters of screens that are near one another.
+    /// Islands that are still too large will be subdivided using a fixed grid
     #[default]
     Islands,
-    /// Break the map up using a fixed grid
+    /// Divide the map into a fixed grid
     Grid,
 }
 
 #[derive(Args)]
 pub struct IslandsArgs {
-    /// The number of empty screens allowed between the screens of an island. -g sets min and max to the same value
+    /// The number of empty screens allowed between the screens of an island.
+    /// -g sets min and max to the same value
     #[arg(short = 'g', long, default_value = "20", help_heading = "Islands partitioner")]
     pub max_gap: u64,
-    /// If islands are too big, max gap will be reduced stepwise until they fit, but no lower than this value.
+    /// If an island is too big, it will be subdivided by gradually reducing max gap no lower than this value.
     /// -g sets min and max to the same value
     #[arg(short = 'g', long, default_value = "1", help_heading = "Islands partitioner")]
     pub min_gap: u64,
@@ -66,10 +69,12 @@ pub struct IslandsArgs {
 
 #[derive(Args)]
 pub struct GridArgs {
-    /// The number of rows to divide the level into. Leave blank to calculate from max height
+    /// The number of rows to divide the level into.
+    /// If unspecified, it will be calculated from max height
     #[arg(short, long, help_heading = "Grid partitioner")]
     pub rows: Option<u64>,
-    /// The number of columns to divide the level into. Leave blank to calculate from max width
+    /// The number of columns to divide the level into.
+    /// If unspecified, it will be calculated from max width
     #[arg(short, long, help_heading = "Grid partitioner")]
     pub cols: Option<u64>,
 }
