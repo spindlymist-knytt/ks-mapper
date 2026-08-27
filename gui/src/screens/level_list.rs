fea4cbe991a5eaafc85f6494d7ec429aedb7b8ea
spindlymist <ocrobin@gmail.com>
2026-08-26 21:43:26 -0700

    Keyboard controls for level list

--- gui/src/screens/level_list.rs
+++ gui/src/screens/level_list.rs
@@ -1,12 +1,13 @@
 use std::{fs, path::{Path, PathBuf}};
 
-use imgui_app::{Extras, dear_imgui_rs::{MouseButton, SelectableFlags, StyleColor, StyleVar}};
+use imgui_app::{Extras, dear_imgui_rs::{Key, MouseButton, SelectableFlags, StyleColor}};
 use imgui_app::dear_imgui_rs::{Condition, Ui, TableFlags, TableSizingPolicy, WindowFlags};
 
 pub struct State {
     levels: Vec<LevelListItem>,
     filter: String,
     selected_index: usize,
+    get_focus: bool,
 }
 
 struct LevelListItem {
@@ -24,13 +25,14 @@ impl State {
             levels,
             filter: String::new(),
             selected_index: 0,
+            get_focus: true,
         }
     }
 }
 
 pub fn build_ui(ui: &Ui, ex: &mut Extras, state: &mut State) -> Option<PathBuf> {
     let (width, height) = ex.window.size();
-    let open_level = ui.window("Main")
+    let open_level = ui.window("Level List")
         .position([0.0, 0.0], Condition::Always)
         .size([width as f32, height as f32], Condition::Always)
         .flags(WindowFlags::NO_TITLE_BAR | WindowFlags::NO_MOVE | WindowFlags::NO_RESIZE)
@@ -39,15 +41,18 @@ pub fn build_ui(ui: &Ui, ex: &mut Extras, state: &mut State) -> Option<PathBuf> 
         
         // Filter
         {
-            let spacing_y = unsafe { ui.style().item_spacing()[1] };
-            let _spacing = ui.push_style_var(StyleVar::ItemSpacing([spacing_y, spacing_y]));
+            let inner_spacing_x = unsafe { ui.style().item_inner_spacing()[0] };
             
             let mut hint_color = ui.style_color(StyleColor::Text);
             hint_color[3] *= 0.25;
-            let _hint_color_token = ui.push_style_color(StyleColor::TextDisabled, hint_color);
+            let _token = ui.push_style_color(StyleColor::TextDisabled, hint_color);
             
+            if state.get_focus {
+                ui.set_keyboard_focus_here();
+                state.get_focus = false;
+            }
             let full_width = ui.content_region_avail_width();
-            let input_width = full_width * (10.0 / 12.0) - spacing_y;
+            let input_width = full_width * (10.0 / 12.0) - inner_spacing_x;
             ui.set_next_item_width(input_width);
             if ui.input_text("##Level filter", &mut state.filter)
                 .hint("Filter levels")
@@ -56,8 +61,50 @@ pub fn build_ui(ui: &Ui, ex: &mut Extras, state: &mut State) -> Option<PathBuf> 
                 filter_levels(&mut state.levels, &state.filter);
             }
 
-            ui.same_line();
+            ui.same_line_with_spacing(0.0, inner_spacing_x);
             open_level |= ui.button_with_size("Open", [-1.0, 0.0]);
+        }
+        
+        // Keyboard controls
+        open_level |= ui.is_key_pressed(Key::Enter);
+        let mut nudge_selection: isize = 0;
+        if ui.is_key_pressed(Key::UpArrow) {
+            nudge_selection = -1;
+        }
+        else if ui.is_key_pressed(Key::DownArrow) {
+            nudge_selection = 1;
+        }
+        else if ui.is_key_pressed(Key::Home) {
+            nudge_selection = isize::MIN;
+        }
+        else if ui.is_key_pressed(Key::End) {
+            nudge_selection = isize::MAX;
+        }
+        else if ui.is_key_pressed(Key::PageUp) {
+            nudge_selection = -calc_rows_per_page(ui);
+        }
+        else if ui.is_key_pressed(Key::PageDown) {
+            nudge_selection = calc_rows_per_page(ui);
+        }
+        
+        if nudge_selection != 0 {
+            let mut filtered_level_indices = Vec::with_capacity(state.levels.len());
+            let mut selected_index_after_filter: isize = -1;
+            
+            for (i, level) in state.levels.iter().enumerate() {
+                if level.is_visible {
+                    filtered_level_indices.push(i);
+                    if i == state.selected_index {
+                        selected_index_after_filter = filtered_level_indices.len() as isize - 1;
+                    }
+                }
+            }
+            
+            let n_filtered_levels = filtered_level_indices.len() as isize;
+            let new_selected_index = selected_index_after_filter
+                .saturating_add(nudge_selection)
+                .clamp(0, n_filtered_levels - 1);
+            state.selected_index = filtered_level_indices[new_selected_index as usize];
         }
         
         ui.table("##LevelsTable")
@@ -71,10 +118,15 @@ pub fn build_ui(ui: &Ui, ex: &mut Extras, state: &mut State) -> Option<PathBuf> 
                     continue;
                 }
                 
+                let is_selected = state.selected_index == i;
+                if is_selected && nudge_selection != 0 {
+                    ui.set_scroll_here_y(0.5);
+                }
+                
                 ui.table_next_row();
                 ui.table_next_column();
                 if ui.selectable_config(&level.display_name)
-                    .selected(state.selected_index == i)
+                    .selected(is_selected)
                     .flags(SelectableFlags::SPAN_ALL_COLUMNS)
                     .build()
                 {
@@ -101,6 +153,12 @@ pub fn build_ui(ui: &Ui, ex: &mut Extras, state: &mut State) -> Option<PathBuf> 
     }
 }
 
+fn calc_rows_per_page(ui: &Ui) -> isize {
+    let table_height = ui.content_region_avail_height();
+    let row_height = ui.text_line_height() + 2.0 * unsafe { ui.style().cell_padding()[1] };
+    (table_height / row_height) as isize
+}
+
 fn list_levels(worlds_dir: impl AsRef<Path>) -> anyhow::Result<Vec<LevelListItem>> {
     let mut levels = Vec::new();
     
@@ -111,7 +169,7 @@ fn list_levels(worlds_dir: impl AsRef<Path>) -> anyhow::Result<Vec<LevelListItem
         }
         
         let level_path = entry.path();
-        let level_name = entry.file_name().to_string_lossy().into_owned();
+        let level_name = entry.file_name().display().to_string();
         
         let map_path = level_path.join("Map.bin");
         let ini_path = level_path.join("World.ini");
